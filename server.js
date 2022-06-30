561eee70ac99b230885801bde7170b94f22d79f4
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-06-30 14:37:19 +0530

    removed console.log and url formating

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const http = require('http');
-const { URL } = require('url');
 
 const startServer = (port, handler) => {
   const server = http.createServer((request, response) => {
-
-    request.url = new URL(`http://localhost:${port}${request.url}`);
-
-    console.log(request.method, request.url.pathname);
     handler(request, response);
   });
-
   server.listen(port, () => console.log(`server listening on ${port}`));
 };
 
 module.exports = { startServer };
-
